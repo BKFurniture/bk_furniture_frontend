75e8c9a466325e42c96435515e971021b06a7fa6
phamanhthien1 <62327329+phamanhthien1@users.noreply.github.com>
2023-04-16 20:58:33 +0700

    fixed mailer

--- src/api/mailer.js
+++ src/api/mailer.js
@@ -1,12 +1,8 @@
 import axios from 'axios'
 
-const DOMAIN_MAILER = 'https://localhost:8081'
+const DOMAIN_MAILER = 'http://localhost:8081'
 const axiosMailer = axios.create({
   baseURL: DOMAIN_MAILER,
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
 })
 export function mailerSignUp({username, email}) {
   return axiosMailer.get(`/mailer/${email}/${username}/sign-up`)
